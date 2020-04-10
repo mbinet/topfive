8ad0d60b06772fce5172288d07334c33dc9c6edf
mbinet <maxime.binet@qonto.eu>
2020-04-10 22:40:50 +0800

    Add logos

--- src/components/posts/PostBlock.jsx
+++ src/components/posts/PostBlock.jsx
@@ -1,8 +1,16 @@
 import PropTypes from "prop-types";
 import Paper from "@material-ui/core/Paper";
+import styled from "styled-components";
 import PostItem from "./PostItem";
 
-const PostBlock = ({ error, isLoaded, posts }) => {
+const Img = styled.img`
+  display: flex;
+  height: 40px;
+  margin: auto;
+  margin-bottom: 16px;
+`;
+
+const PostBlock = ({ error, image, isLoaded, posts }) => {
   if (error) {
     return <div>Erreur : {error.message}</div>;
   }
@@ -10,25 +18,31 @@ const PostBlock = ({ error, isLoaded, posts }) => {
     return <div>Chargement… jrigole</div>;
   }
   return (
-    <Paper elevation={6}>
-      {posts.map((post) => (
-        <PostItem
-          discussionUrl={post.discussionUrl}
-          imageUrl={post.imageUrl}
-          key={post.id}
-          url={post.url}
-          points={post.points}
-          subtitle={post.subtitle}
-          title={post.title}
-        />
-      ))}
-    </Paper>
+    <div>
+      <Img src={image.src} alt={image.alt} />
+      <Paper elevation={6}>
+        {posts.map((post) => (
+          <PostItem
+            discussionUrl={post.discussionUrl}
+            imageUrl={post.imageUrl}
+            key={post.id}
+            url={post.url}
+            points={post.points}
+            subtitle={post.subtitle}
+            title={post.title}
+          />
+        ))}
+      </Paper>
+    </div>
   );
 };
 
 PostBlock.propTypes = {
   error: PropTypes.objectOf(Error),
-  isLoaded: PropTypes.bool.isRequired,
+  image: PropTypes.shape({
+    src: PropTypes.string,
+    alt: PropTypes.string,
+  }).isRequired,
   posts: PropTypes.arrayOf(
     PropTypes.shape({
       discussionUrl: PropTypes.string,
@@ -40,6 +54,7 @@ PostBlock.propTypes = {
       url: PropTypes.string,
     })
   ),
+  isLoaded: PropTypes.bool.isRequired,
 };
 
 PostBlock.defaultProps = {
